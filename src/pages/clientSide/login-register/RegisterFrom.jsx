e7cb9b3230b1dcf84f4ba1661c35b68e04d21767
ishanrana363 <ishanrana363@gmail.com>
2025-01-25 17:43:05 +0600

    user registration completed

--- src/pages/clientSide/login-register/RegisterFrom.jsx
+++ src/pages/clientSide/login-register/RegisterFrom.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+import { createAlert } from "../../../helper/createAlert";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
 
 const RegistrationForm = () => {
     const [passwordType, setPasswordType] = useState("password");
@@ -15,6 +18,64 @@ const RegistrationForm = () => {
         }
     };
 
+    const [loading, setLoading] = useState(false);
+    const axiosPublic = useAxiosPublic()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const confirmPassword = e.target.confirmPassword.value;
+        const payload = {
+            name,
+            email,
+            password,
+            confirmPassword
+        }
+        if (password !== confirmPassword) {
+            Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Password & Confiremd Password Not Match",
+                showConfirmButton: false,
+                timer: 1500
+            })
+        }
+
+        try {
+            let resp = await createAlert();
+            if (resp.isConfirmed) {
+                setLoading(true);
+                let res = await axiosPublic.post(`/user`, payload);
+                setLoading(false);
+                if (res) {
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Registration sccessfully",
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                    e.target.reset();
+                }
+            }
+        } catch (error) {
+            setLoading(false);
+            Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Registration to failed",
+                showConfirmButton: false,
+                timer: 1500
+            })
+
+        }
+
+
+
+    }
+
 
 
     return (
@@ -23,7 +84,7 @@ const RegistrationForm = () => {
                 <title>Glorious | Registration Page</title>
             </Helmet>
             <h2 className="text-2xl font-bold text-center mb-6">Registration From</h2>
-            <form>
+            <form onSubmit={handleSubmit} >
                 <div className="mb-4">
                     <label htmlFor="name" className="block text-gray-700 font-medium mb-1">
                         Name
@@ -31,8 +92,10 @@ const RegistrationForm = () => {
                     <input
                         type="text"
                         id="name"
+                        name="name"
                         className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
                         placeholder="Enter your name"
+                        required
                     />
                 </div>
                 <div className="mb-4">
@@ -42,8 +105,10 @@ const RegistrationForm = () => {
                     <input
                         type="email"
                         id="email"
+                        name="email"
                         className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
                         placeholder="Enter your email"
+                        required
                     />
                 </div>
                 <div className="mb-4 relative">
@@ -52,9 +117,11 @@ const RegistrationForm = () => {
                     </label>
                     <input
                         type={passwordType}
+                        name="password"
                         id="password"
                         className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
                         placeholder="Enter your password"
+                        required
                     />
                     <button
                         type="button"
@@ -71,6 +138,7 @@ const RegistrationForm = () => {
                     <input
                         type={confirmPasswordType}
                         id="confirm-password"
+                        name="confirmPassword"
                         className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
                         placeholder="Confirm your password"
                     />
@@ -86,7 +154,9 @@ const RegistrationForm = () => {
                     type="submit"
                     className="w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-300"
                 >
-                    Register
+                    {
+                        loading ? "Submiting..." : "Register"
+                    }
                 </button>
                 <Link className="block mt-2" to={`/login`}>You Have Already Account? <span className="hover:underline" >LoginIn</span> </Link>
             </form>
