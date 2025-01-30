15635d655a287e397a9999dc42d31359226a18d7
ishanrana363 <ishanrana363@gmail.com>
2025-01-30 10:22:07 +0600

    some changes

--- src/components/clientSide/floatingBtn/FloatingBtn.jsx
+++ src/components/clientSide/floatingBtn/FloatingBtn.jsx
@@ -2,24 +2,24 @@ import React from 'react';
 import { BsWhatsapp } from 'react-icons/bs';
 import { FaPhone, FaWhatsapp } from 'react-icons/fa';
 
-const FloatingBtn = ({phone, whatsapp}) => {
+const FloatingBtn = ({ phone, whatsapp }) => {
     return (
         <div>
             <div className="fixed bottom-4 left-4 flex gap-4 w-11/12 mx-auto justify-between">
                 {/* Phone Call Button */}
                 <button className="bg-green-500 p-3 rounded-full text-white shadow-lg hover:bg-green-600 transition duration-300">
                     <a
-                        href={`tel:+88${phone}`}
+                        href={`tel:+8801710681347`}
                         className="flex items-center text-white text-xs sm:text-base flex-col"
                     >
                         <FaPhone size={24} />
                     </a>
                 </button>
-                
+
                 {/* WhatsApp Button */}
                 <button className="bg-green-500 p-3 rounded-full text-white shadow-lg hover:bg-green-600 transition duration-300">
                     <a
-                        href={`https://wa.me/+88${whatsapp}`} 
+                        href={`https://wa.me/+8801606521176 `}
                         className="text-white text-lg sm:text-2xl"
                         target="_blank"
                         rel="noopener noreferrer"
